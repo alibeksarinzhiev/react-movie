87ee00e0226a2e55b3d665b33cf77a46fdbfb351
alibek <alibek1024@mail.ru>
2023-07-07 20:55:28 +0600

    film

--- src/Pages/Home/trailer/trailer.js
+++ src/Pages/Home/trailer/trailer.js
@@ -1,5 +1,7 @@
 import React, {useContext} from 'react';
 import {CustomContext} from "../../../Context";
+import "./trailer.scss"
+import fastandfurios from './fastandfurios.png'
 
 const Trailer = () => {
     const {movies} = useContext(CustomContext)
@@ -7,6 +9,8 @@ const Trailer = () => {
         <section className='trailer'>
             <div className="trailer__container container">
                 <h2>Новые трейлеры</h2>
+                <h2>Все трейлеры</h2>
+                {/*<img src={fastandfurios} alt=""/>*/}
                 <div className="trailer__box">
                     {movies.map((el)=>(
                         <div key={el.id} className='trailer__card'>
